d117a6ae9a134dfa11abb875bbd3a125af0cb0f5
sleepyrave <rockquila@gmail.com>
2025-03-26 12:47:32 +0800

    first commit

---
+++ server/app/Models/Gender.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+class Gender extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'tbl_genders';
+    protected $primaryKey = 'gender_id';
+    protected $fillable = [
+        'gender',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'gender_id', 'gender_id');
+    }
+}
